8e0a7b570a26dbd0c9d1d1f34cfd84c7c9359a7f
sg22049 <sg22049@gmail.com>
2022-08-14 21:54:41 +0900

    1018~1019 완료

---
+++ chap2-입출력문 및 연산자/src/com/codeup/basic/Test1018.java
@@ -0,0 +1,15 @@
+package com.codeup.basic;
+
+import java.util.Scanner;
+
+public class Test1018 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+	
+		String time = sc.next();
+		System.out.println(time);
+	}
+
+}
