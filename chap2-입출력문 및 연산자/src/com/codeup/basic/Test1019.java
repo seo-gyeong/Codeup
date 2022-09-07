f2dab985ff14acb716f2a90d831635188ea671fd
sg220 <sg220@DESKTOP-1PH9K1L>
2022-09-07 14:04:14 +0900

    text -> text 오타 수정

--- chap2-입출력문 및 연산자/src/com/codeup/basic/Test1019.java
+++ chap2-입출력문 및 연산자/src/com/codeup/basic/Test1019.java
@@ -2,7 +2,7 @@ package com.codeup.basic;
 
 import java.util.Scanner;
 
-public class Test1019 { 
+public class Test1019 {   
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
