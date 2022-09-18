6ede04352ceda7d8796420f0aa0f3948a41fb2a3
sg220 <sg220@DESKTOP-1PH9K1L>
2022-09-18 16:08:29 +0900

    1131~1133 / 1135~1137 완료

---
+++ chap2-입출력문 및 연산자/src/com/codeup/basic/Test1131.java
@@ -0,0 +1,15 @@
+package com.codeup.basic;
+
+import java.util.Scanner;
+
+public class Test1131 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		char a = sc.next().charAt(0);
+		System.out.println(a);
+
+	}
+
+}
